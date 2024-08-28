f0c80176f0fef0f2b5a074e2fc6803bb13809fda
venomous-maker <morganokumu8+1@gmail.com>
2024-08-28 23:56:23 +0300

    Added user overview widget

---
+++ app/Filament/Resources/UserResource/Widgets/UsersOverview.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Widgets;
+
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class UsersOverview extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            //
+        ];
+    }
+}
