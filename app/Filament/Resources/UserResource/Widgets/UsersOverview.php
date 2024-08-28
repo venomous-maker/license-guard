c341b1f096adfa8eee8d0baf137c375dbe438644
venomous-maker <morganokumu8+1@gmail.com>
2024-08-28 23:58:57 +0300

    Added user overview stats

--- app/Filament/Resources/UserResource/Widgets/UsersOverview.php
+++ app/Filament/Resources/UserResource/Widgets/UsersOverview.php
@@ -2,15 +2,22 @@
 
 namespace App\Filament\Resources\UserResource\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use App\Models\User;
+use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class UsersOverview extends BaseWidget
+class UsersOverview extends StatsOverviewWidget
 {
     protected function getStats(): array
     {
         return [
             //
-        ];
+            Stat::make('Number of Users', User::query()->withTrashed()->count())->description('All Users registered on this Application')->icon('heroicon-o-user-group'),
+            Stat::make('Verified Users', User::query()->withTrashed()->whereNotNull('email_verified_at')->count())
+                ->description('All Users with Verified Accounts')->icon('lineawesome-user-check-solid'),
+            Stat::make('Deleted Users', User::onlyTrashed()->count())
+                ->description('Number of users who have been soft deleted')
+                ->icon('forkawesome-user-times'),
+            ];
     }
 }
